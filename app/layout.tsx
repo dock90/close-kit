301a07e12030d3a299bcf9a068793add31cddb80
Edward Darling <edward@dock90.io>
2025-10-23 12:01:52 -0600

    Fix viewport issues

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,4 @@
-import type { Metadata } from 'next';
+import type { Metadata, Viewport } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
 import { ClerkProvider } from '@clerk/nextjs';
@@ -15,11 +15,12 @@ export const metadata: Metadata = {
 		statusBarStyle: 'default',
 		title: 'CloseKit',
 	},
-	viewport: {
-		width: 'device-width',
-		initialScale: 1,
-		maximumScale: 1,
-	},
+};
+
+export const viewport: Viewport = {
+	width: 'device-width',
+	initialScale: 1,
+	maximumScale: 1,
 };
 
 export default function RootLayout({
